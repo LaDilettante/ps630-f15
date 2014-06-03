acfcadfc4f0c4b8cf20fd88c5cfc88d830dfe4b5
Anh Le <aql3@duke.edu>
2014-06-03 16:10:47 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## My Teaching Assistant App
+
+Some functions:
+* Take homework then randomly sends out to grader
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TAApp::Application.config.secret_key_base = '45a9edb6fb446e02f22c3b21da8c1f83cf86cf9a4158cca48c8dd2c264decb6bca20a8fbcf7f8b83de0d0ba1ca5558e130e5061cccbdc45b15c2ef31d3c47e88'
