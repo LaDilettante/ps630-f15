01ec5b2f915e6dd8a47cdd2dc701bea83c58e67a
Anh Le <aql3@duke.edu>
2014-07-18 10:52:40 -0400

    change development mailer to gmail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,7 +27,20 @@ TAApp::Application.configure do
   # number of complex assets.
   config.assets.debug = true
 
-  ActionMailer::Base.default :from => 'app27277259@heroku.com'
   # Open email in browser
-  config.action_mailer.delivery_method = :letter_opener
+  #config.action_mailer.delivery_method = :letter_opener
+  
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: "587",
+    domain: "gmail.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV['GMAIL_USERNAME_DEV'],
+    password: ENV['GMAIL_PASSWORD_DEV']
+  }
 end
