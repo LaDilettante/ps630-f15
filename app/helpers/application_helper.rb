9e909c3ecae5ac008a9b2f8eebbde8d454117592
Anh Le <aql3@duke.edu>
2014-08-26 23:03:08 -0400

    change app name in applicationhelper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
   def full_title(page_title)
-    base_title = "My Teaching Assistant App"
+    base_title = "POLSCI 630"
     if page_title.empty?
       base_title
     else
