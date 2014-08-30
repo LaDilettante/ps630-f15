b45715e7b6f31c13138893a846cc5df002ad36de
Anh Le <aql3@duke.edu>
2014-08-30 17:20:56 -0400

    change s3 header content-disposition to attachment. So the file will be downloaded instead of displayed in browser

--- app/models/meeting_material.rb
+++ app/models/meeting_material.rb
@@ -1,14 +1,7 @@
 class MeetingMaterial < ActiveRecord::Base
   belongs_to :meeting
-  has_attached_file :material
+  has_attached_file :material,
+    s3_headers: { 'Content-Disposition' => "attachment" }
 
   validates_attachment :material, size: { in: 0..10.megabytes }
-
-  before_post_process :set_content_type
-
-  private
-
-    def set_content_type
-      self.material.instance_write(:content_type, MIME::Types.type_for(self.material.queued_for_write[:original].file_name).to_s)
-    end
 end
