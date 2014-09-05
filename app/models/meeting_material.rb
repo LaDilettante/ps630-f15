532b42f36d27470bb2b747ee9bb578e6b0ef3812
Anh Le <aql3@duke.edu>
2014-09-05 18:36:41 -0400

    add checkbox and model delete

--- app/models/meeting_material.rb
+++ app/models/meeting_material.rb
@@ -2,5 +2,8 @@ class MeetingMaterial < ActiveRecord::Base
   belongs_to :meeting
   has_attached_file :material
 
+  attr_accessor :delete_material
+  before_validation { material.clear if delete_material == '1' }
+
   validates_attachment :material, size: { in: 0..10.megabytes }
 end
